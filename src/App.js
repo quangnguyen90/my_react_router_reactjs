c8fda62c6047d3e08237a11c92eb139429d28c66
quangnguyen90 <nguyenphuquang90@gmail.com>
2020-09-13 22:30:31 +0700

    2- Integrate BrowserRoute & Route from react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App" >
+          {/* Menu */}
+          <nav className="navbar navbar-inverse">
+            <ul className="nav navbar-nav">
+              <li className="active">
+                <a href="/#">Homepage</a>
+              </li>
+              <li>
+                <a href="/#">About</a>
+              </li>
+            </ul>
+          </nav>
+
+          {/* Content */}
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+        </div>
+      </Router>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/About.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+class About extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {};
+
+    }
+
+    render() {
+        return (
+            <div>
+                This is About page
+            </div>
+        );
+    }
+}
+
+About.propTypes = {
+
+};
+
+export default About;
