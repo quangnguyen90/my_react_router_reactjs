e3570fa420b588e5d4c87935dffabb3fd7db4be2
Quang Nguyen Phu <quang.nguyenphu@grevo.net>
2020-09-14 13:39:30 +0700

    7.1- Show active menu link

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,10 +1,28 @@
 import React, { Component } from "react";
 import { Route, Link } from 'react-router-dom';
 
+const menus = [
+    {
+        name: 'Home',
+        to: '/',
+        exact: true
+    },
+    {
+        name: 'About',
+        to: '/about',
+        exact: false
+    },
+    {
+        name: 'Contact',
+        to: '/contact',
+        exact: false
+    }
+];
+
 const MenuLink = ({ label, to, activeOnlyWhenExact }) => {
     return (
         <Route path={to} exact={activeOnlyWhenExact} children={({ match }) => {
-            var active = match ? 'active_link' : '';
+            var active = match ? 'active lizk' : '';
             return (
                 <li className={active}>
                     <Link to={to} className="my-link">
@@ -28,12 +46,27 @@ class Menu extends Component {
         return (
             <nav className="navbar navbar-default">
                 <ul className="nav navbar-nav">
-                    <MenuLink label="Home" to="/" activeOnlyWhenExact={true} />
-                    <MenuLink label="About" to="/about" activeOnlyWhenExact={false} />
-                    <MenuLink label="Contact" to="/contact" activeOnlyWhenExact={false} />
+                    { this.showMenus(menus) }
                 </ul>
             </nav>
         );
+    }
+
+    showMenus = (menus) => {
+        var result = null;
+        if (menus.length > 0) {
+            result = menus.map((menu, index) => {
+                return (
+                    <MenuLink
+                        key={index}
+                        label={menu.name}
+                        to={menu.to}
+                        activeOnlyWhenExact={menu.exact}
+                    />
+                )
+            });
+        }
+        return result;
     }
 }
 
