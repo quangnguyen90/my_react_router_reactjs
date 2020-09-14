bbd63765f2d9f7b930d89b6c406d2ac0be545706
Quang Nguyen Phu <quang.nguyenphu@grevo.net>
2020-09-14 15:12:39 +0700

    9- Prompt

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,17 +1,33 @@
 import React, { Component } from "react";
-
+import { Prompt } from 'react-router-dom';
 class Contact extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {};
+        this.state = {
+            isChecked : false
+        };
 
     }
 
+    onClick = (isChecked) => {
+        this.setState({
+            isChecked: isChecked
+        })
+    }
+
     render() {
+        var {isChecked} = this.state;
         return (
             <div>
                 This is Contact page
+                <br/>
+                <button type="button" className="btn btn-info" onClick={() => this.onClick(false)}>Allow</button>
+                <button type="button" className="btn btn-danger" onClick={() => this.onClick(true)}>Not Allow</button>
+                <Prompt
+                    when={isChecked}
+                    message={location => (`Are you sure you want to go to page: ${location.pathname}`)}
+                />
             </div>
         );
     }
